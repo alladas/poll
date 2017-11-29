7e47bb1f04c39e79dc6b3ea15934595960be6ed6
heckslay <lashaa.lomidze@gmail.com>
2017-11-29 14:31:21 +0400

    remove unnecesarry columns from migrations

--- migrations/create/m171129_061747_add_user_interaction_related_columns_to_polls_related_tables.php
+++ migrations/create/m171129_061747_add_user_interaction_related_columns_to_polls_related_tables.php
@@ -7,64 +7,14 @@ class m171129_061747_add_user_interaction_related_columns_to_polls_related_table
     public function safeUp()
     {
         $this->addColumn('{{%polls}}', 'deleted_at', $this->integer(11));
-        $this->addColumn('{{%polls}}', 'deleted_by', $this->integer(11));
         $this->addColumn('{{%polls_answers}}', 'deleted_at', $this->integer(11));
-        $this->addColumn('{{%polls_answers}}', 'deleted_by', $this->integer(11));
         $this->addColumn('{{%polls_result}}', 'deleted_at', $this->integer(11));
-        $this->addColumn('{{%polls_result}}', 'deleted_by', $this->integer(11));
-        $this->addColumn('{{%polls}}', 'created_at', $this->integer(11));
-        $this->addColumn('{{%polls}}', 'created_by', $this->integer(11));
-        $this->addColumn('{{%polls_answers}}', 'created_at', $this->integer(11));
-        $this->addColumn('{{%polls_answers}}', 'created_by', $this->integer(11));
-        $this->addColumn('{{%polls_result}}', 'created_at', $this->integer(11));
-        $this->addColumn('{{%polls_result}}', 'created_by', $this->integer(11));
-        $this->addColumn('{{%polls}}', 'updated_at', $this->integer(11));
-        $this->addColumn('{{%polls}}', 'updated_by', $this->integer(11));
-        $this->addColumn('{{%polls_answers}}', 'updated_at', $this->integer(11));
-        $this->addColumn('{{%polls_answers}}', 'updated_by', $this->integer(11));
-        $this->addColumn('{{%polls_result}}', 'updated_at', $this->integer(11));
-        $this->addColumn('{{%polls_result}}', 'updated_by', $this->integer(11));
-
-        $this->addForeignKey('fk_polls_deleted_by','{{%polls}}','deleted_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_answer_deleted_by','{{%polls_answers}}','deleted_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_result_deleted_by','{{%polls_result}','deleted_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_created_by','{{%polls}}','created_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_answer_created_by','{{%polls_answers}}','created_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_result_created_by','{{%polls_result}','created_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_updated_by','{{%polls}}','updated_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_answer_updated_by','{{%polls_answers}}','updated_by','{{%user}}', 'id');
-        $this->addForeignKey('fk_polls_result_updated_by','{{%polls_result}','updated_by','{{%user}}', 'id');
     }
 
     public function safeDown()
     {
-        $this->dropForeignKey('fk_polls_result_updated_by','{{%polls_result}}');
-        $this->dropForeignKey('fk_polls_answer_updated_by','{{%polls_answers}}');
-        $this->dropForeignKey('fk_polls_updated_by','{{%polls}}');
-        $this->dropForeignKey('fk_polls_result_created_by','{{%polls_result}');
-        $this->dropForeignKey('fk_polls_answer_created_by','{{%polls_answers}}');
-        $this->dropForeignKey('fk_polls_created_by','{{%polls}}');
-        $this->dropForeignKey('fk_polls_result_deleted_by','{{%polls_result}');
-        $this->dropForeignKey('fk_polls_answer_deleted_by','{{%polls_answers}}');
-        $this->dropForeignKey('fk_polls_deleted_by','{{%polls}}');
-
-        $this->dropColumn('{{%polls_result}}','updated_by');
-        $this->dropColumn('{{%polls_result}}','updated_at');
-        $this->dropColumn('{{%polls_answers}}','updated_by');
-        $this->dropColumn('{{%polls_answers}}','updated_at');
-        $this->dropColumn('{{%polls}}','updated_by');
-        $this->dropColumn('{{%polls}}','updated_at');
-        $this->dropColumn('{{%polls_result}}','created_by');
-        $this->dropColumn('{{%polls_result}}','created_at');
-        $this->dropColumn('{{%polls_answers}}','created_by');
-        $this->dropColumn('{{%polls_answers}}','created_at');
-        $this->dropColumn('{{%polls}}','created_by');
-        $this->dropColumn('{{%polls}}','created_at');
-        $this->dropColumn('{{%polls_result}}', 'deleted_by');
         $this->dropColumn('{{%polls_result}}', 'deleted_at');
-        $this->dropColumn('{{%polls_answers}}', 'deleted_by');
         $this->dropColumn('{{%polls_answers}}', 'deleted_at');
-        $this->dropColumn('{{%polls}}', 'deleted_by');
         $this->dropColumn('{{%polls}}', 'deleted_at');
     }
 
