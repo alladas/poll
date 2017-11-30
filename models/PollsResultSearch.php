5a4af5b2b230012e99d2f075c69195b42e7e5d6c
heckslay <lashaa.lomidze@gmail.com>
2017-11-30 14:04:38 +0400

    Update the query to select through polls answers except polls results to get all not deleted answer names in final result

--- models/PollsResultSearch.php
+++ models/PollsResultSearch.php
@@ -9,14 +9,16 @@ use lslsoft\poll\models\PollsResult;
 /**
  * PollsResultSearch represents the model behind the search form about `backend\models\PollsResult`.
  */
-class PollsResultSearch extends PollsResult {
+class PollsResultSearch extends PollsResult
+{
 
     /**
      * @inheritdoc
      */
     public $answer;
 
-    public function rules() {
+    public function rules()
+    {
         return [
             [['num', 'id_poll', 'id_answer'], 'integer'],
             [['answer'], 'safe']
@@ -26,7 +28,8 @@ class PollsResultSearch extends PollsResult {
     /**
      * @inheritdoc
      */
-    public function scenarios() {
+    public function scenarios()
+    {
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
     }
@@ -38,14 +41,21 @@ class PollsResultSearch extends PollsResult {
      *
      * @return ActiveDataProvider
      */
-    public function search($params) {
+    public function search($params)
+    {
+//        $query = PollsResult::find()
+//            ->select('id_answer, count(`id_answer`) as `res`')
+//            ->joinWith('idAnswer')
+//            ->groupBy(['id_answer'])
+//            ->where(['polls_result.id_poll' => $params['id_poll']]);
 
-        $query = PollsResult::find()
-                ->select('id_answer, count(`id_answer`) as `res`')               
-                ->joinWith('idAnswer')
-                ->groupBy(['id_answer'])
-                ->where(['polls_result.id_poll'=>$params['id_poll']]);
-                
+
+        $query = PollsAnswers::find()
+            ->select('polls_answers.answer,polls_answers.id, count(polls_result.id) as `res`')
+            ->leftJoin('polls_result', 'id_answer = polls_answers.id')
+            ->groupBy([PollsAnswers::tableName() . '.id'])
+            ->where(['polls_answers.id_poll' => $params['id_poll']])->andWhere(['polls_answers.deleted_at' => null]);
+
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
